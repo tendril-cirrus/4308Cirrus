b0a3a604a997d4be8d98474d8aadf734d000ea2f
Dan Richelson <drichelson@gmail.com>
2011-11-29 18:22:59 -0700

    working screen to get current meter readings

--- src/cx/it/cirrus/TendrilManager.java
+++ src/cx/it/cirrus/TendrilManager.java
@@ -1,13 +1,29 @@
 package cx.it.cirrus;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class TendrilManager extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class TendrilManager extends Activity implements OnClickListener {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		View currentMeterReading = findViewById(R.id.current_meter_reading);
+		currentMeterReading.setOnClickListener(this);
+	}
+
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.current_meter_reading:
+			startActivity(new Intent(this, ViewCurrentMeterReading.class));
+			break;
+		}
+
+	}
+
 }
---
+++ src/cx/it/cirrus/ViewCurrentMeterReading.java
@@ -0,0 +1,74 @@
+package cx.it.cirrus;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+import android.util.Log;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.regex.Pattern;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class ViewCurrentMeterReading extends Activity {
+	private static final String url = "https://dev-program.tendrildemo.com/api/rest/meter/read;account=Jenkins;from=2010-01-01T00:00:00-0000;limitToLatest=1";
+	private static String note_id = "";
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.current_meter_reading);
+	}
+
+	private void updateCurrentMeterReading() {
+
+		String response = "";
+		String line = "";
+		HttpsURLConnection c = null;
+		try {
+			URL u = new URL(url);
+
+			c = (HttpsURLConnection) u.openConnection();
+
+			c.setRequestProperty("Emsauthtoken",
+					"jason@tendrilinc.com:password");
+			c.setRequestProperty("Accept", "application/json");
+
+			c.setRequestMethod("GET");
+			c.setDoOutput(true);
+			c.connect();
+
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					c.getInputStream()));
+			while ((line = reader.readLine()) != null) {
+				response += line;
+			}
+		} catch (MalformedURLException e) {
+			Log.e(this.getLocalClassName(),
+					"MalformedURLException  " + e.toString());
+		} catch (IOException e) {
+			Log.e(this.getLocalClassName(), "IOException   " + e.toString());
+			e.printStackTrace();
+		} finally {
+			if (c != null)
+				c.disconnect();
+		}
+
+		TextView v = (TextView) findViewById(R.id.textView1);
+		v.setText(response);
+	}
+
+
+	public void onStart() {
+		super.onStart();
+		updateCurrentMeterReading();
+	}
+}
