0823fdc0d419b56bcd774817818ded11161cd6b6
Dan Richelson <drichelson@gmail.com>
2011-12-06 16:50:43 -0700

    refeactored main page- date intervals now work and return json objects

--- src/cx/it/cirrus/MeterUtils.java
+++ src/cx/it/cirrus/MeterUtils.java
@@ -3,6 +3,10 @@ package cx.it.cirrus;
 import java.util.Calendar;
 
 import android.util.Log;
+
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -16,57 +20,112 @@ import java.util.regex.Pattern;
 
 import javax.net.ssl.HttpsURLConnection;
 
+
+
 public class MeterUtils {
-    private static final String baseMeterReadURL = "https://dev-program.tendrildemo.com/api/rest/meter/read;account=Jenkins";
-    private static String note_id = "";
+	public static final String METER_READINGS_BASE_URL = "https://dev-program.tendrildemo.com/api/rest/meter/read;account=Jenkins;";
+	public static final String CONSUMPTION_BASE_URL = "https://dev-program.tendrildemo.com/api/rest/meter/consumption;account=Jenkins;";
+	
+	private static final String username = "jason@tendrilinc.com"; 
+	private static final String password = "password";
+	
+	public static String getCurrentMeterReading() {
 
+		String response = "";
+		String line = "";
+		HttpsURLConnection c = null;
 
-    public static String getCurrentMeterReading(String username, String password) {
+		String currentMeterReadURL = METER_READINGS_BASE_URL
+				+ ";from=2000-01-01T00:00:00-0000;limitToLatest=1";
+		try {
+			URL u = new URL(currentMeterReadURL);
 
-        String response = "";
-        String line = "";
-        HttpsURLConnection c = null;
+			c = (HttpsURLConnection) u.openConnection();
 
-        String currentMeterReadURL = baseMeterReadURL + ";from=2010-01-01T00:00:00-0000;limitToLatest=1";
-        try {
-            URL u = new URL(currentMeterReadURL);
+			c.setRequestProperty("Emsauthtoken", username + ":" + password);
+			c.setRequestProperty("Accept", "application/json");
 
-            c = (HttpsURLConnection) u.openConnection();
+			c.setRequestMethod("GET");
+			c.setDoOutput(true);
+			c.connect();
 
-            c.setRequestProperty("Emsauthtoken",
-                    username+":"+password);
-            c.setRequestProperty("Accept", "application/json");
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					c.getInputStream()));
+			while ((line = reader.readLine()) != null) {
+				response += line;
+			}
+		} catch (MalformedURLException e) {
+			Log.e("MeterUtils ", "MalformedURLException  " + e.toString());
+		} catch (IOException e) {
+			Log.e("MeterUtils ", "IOException   " + e.toString());
+			e.printStackTrace();
+		} finally {
+			if (c != null)
+				c.disconnect();
+		}
 
-            c.setRequestMethod("GET");
-            c.setDoOutput(true);
-            c.connect();
+		return response;
+	}
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(
-                    c.getInputStream()));
-            while ((line = reader.readLine()) != null) {
-                response += line;
-            }
-        } catch (MalformedURLException e) {
-            Log.e("MeterUtils ",
-                    "MalformedURLException  " + e.toString());
-        } catch (IOException e) {
-            Log.e("MeterUtils ", "IOException   " + e.toString());
-            e.printStackTrace();
-        } finally {
-            if (c != null)
-                c.disconnect();
-        }
+	public static JSONObject getDateRangeReadings(String baseURL, String start,
+			String end) throws JSONException {
 
-        return response;
-    }
+		String response = "";
+		String line = "";
+		HttpsURLConnection c = null;
 
-    public static JSONObject getDateRangeMeterReading(String username, String password, Calendar start, Calendar end){
-//        String startDateString = 
+		String currentMeterReadURL = baseURL + ";from="
+				+ start + ";to="
+				+ end
+				+ ";limitToLatest=100";
+		Log.i("url: ", currentMeterReadURL);
+		
+		try {
+			URL u = new URL(currentMeterReadURL);
 
-        return new JSONObject();
+			c = (HttpsURLConnection) u.openConnection();
 
-        
-    }
+			c.setRequestProperty("Emsauthtoken", username + ":" + password);
+			c.setRequestProperty("Accept", "application/json");
 
+			c.setRequestMethod("GET");
+			c.setDoOutput(true);
+			c.connect();
+
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					c.getInputStream()));
+			while ((line = reader.readLine()) != null) {
+				response += line;
+			}
+		} catch (MalformedURLException e) {
+			Log.e("MeterUtils ", "MalformedURLException  " + e.toString());
+		} catch (IOException e) {
+			Log.e("MeterUtils ", "IOException   " + e.toString());
+			e.printStackTrace();
+		} finally {
+			if (c != null)
+				c.disconnect();
+		}
+		//System.out.println(response);
+		return new JSONObject(response);
+
+	}
+//2010-01-01T00:00:00-0000
+	
+//	12-06 16:06:32.253: I/url:(308): https://dev-program.tendrildemo.com/api/rest/meter/read;account=Jenkins;
+	//from=2011-12-06T16:06:16.061-07:00;
+	//to=2011-12-06T16:06:16.062-07:00
+
+	
+	public static String calendarToURLString(Calendar cal, boolean includeTime) {
+		DateTime d = new DateTime(cal);
+		DateTimeFormatter dtf;
+		if (includeTime)
+			dtf = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss-0000");
+		else
+			dtf = DateTimeFormat.forPattern("yyyy-MM-dd-0000");
+		
+		return d.toString(dtf);
+	}
 
 }
--- src/cx/it/cirrus/TendrilManager.java
+++ src/cx/it/cirrus/TendrilManager.java
@@ -15,6 +15,10 @@ import com.androidplot.xy.*;
 import java.text.DecimalFormat;
 import java.util.Arrays;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
 import android.view.View;
 
 import android.view.View.OnClickListener;
@@ -24,118 +28,145 @@ import android.widget.DatePicker;
 import android.widget.Toast;
 
 public class TendrilManager extends Activity implements Button.OnClickListener {
-    private Button startDateButton;
-    private Button endDateButton;
-    private Button refreshButton;
-    static final int START_DATE_DIALOG_ID = 0;
-    static final int END_DATE_DIALOG_ID = 1;
-    private Calendar startDateCalendar = Calendar.getInstance();
-    private Calendar endDateCalendar = Calendar.getInstance();
-    
-    // Zero out the time for the calendars
-    
-    // Listener for startDateButton
-    private DatePickerDialog.OnDateSetListener startDateListener = new DatePickerDialog.OnDateSetListener() {
-        // onDateSet method
-        public void onDateSet(DatePicker view, int year, int monthOfYear,
-                int dayOfMonth) {
-            // Set startDateCalendar
-            startDateCalendar.set(year, monthOfYear, dayOfMonth, 0, 0, 0);
-            System.out.println("Start Date: " + startDateCalendar.toString());
-            
-        }
-    };
-    
-    // Listener fo endDateButton
-    private DatePickerDialog.OnDateSetListener endDateListener = new DatePickerDialog.OnDateSetListener() {
-        // onDateSet method
-        public void onDateSet(DatePicker view, int year, int monthOfYear,
-                int dayOfMonth) {
-            // Set endDateCalendar
-            endDateCalendar.set(year, monthOfYear, dayOfMonth, 23, 59, 59);
-            System.out.println("End Date: " + endDateCalendar.toString());
-            
-        }
-    };
-    
-    // Listener for all other buttons
-    private simpleListener mySimpleListener = new simpleListener();
-    
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        // Adding buttons to the layout
-        startDateButton = (Button) findViewById(R.id.pickStartDate);
-        // add a click listener to the button
-        startDateButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                showDialog(START_DATE_DIALOG_ID);
-            }
-        });
-        
-        endDateButton = (Button) findViewById(R.id.pickEndDate);
-        endDateButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                showDialog(END_DATE_DIALOG_ID);
-            }
-        });
-    }
-    
-    class simpleListener implements OnClickListener {
-        // 8 visibility = gone
-        // 4 visibility = invisible
-        // 0 visibility = visible
-        @Override
-        public void onClick(View v) {
-            // Refresh the graph and current reading
-            if (v.equals(refreshButton)) {
-                refreshGraph();
-                refreshCurrent();
-            } else {
-                // No action defined for whatever button called us
-                // Notify with a Toast Popup
-                Toast.makeText(TendrilManager.this,
-                        "No action defined for button: " + v.toString(),
-                        Toast.LENGTH_SHORT).show();
-                
-            }
-        }
-    }
-    
-    private void refreshGraph() {
-        
-    }
-    
-    private void refreshCurrent() {
-        
-    }
-    
-    // Creating dialog
-    @Override
-    protected Dialog onCreateDialog(int id) {
-        Calendar c = Calendar.getInstance();
-        int cyear = c.get(Calendar.YEAR);
-        int cmonth = c.get(Calendar.MONTH);
-        int cday = c.get(Calendar.DAY_OF_MONTH);
-        switch (id) {
-        case START_DATE_DIALOG_ID:
-            return new DatePickerDialog(this, startDateListener, cyear, cmonth,
-                    cday);
-        case END_DATE_DIALOG_ID:
-            return new DatePickerDialog(this, endDateListener, cyear, cmonth,
-                    cday);
-        }
-        return null;
-    }
-    
-    @Override
-    public void onClick(View v) {
-        if (v == startDateButton) {
-            showDialog(START_DATE_DIALOG_ID);
-        } else if (v == endDateButton) {
-            showDialog(END_DATE_DIALOG_ID);
-        }
-    }
-    
+	private Button startDateButton;
+	private Button endDateButton;
+	private Button refreshButton;
+	static final int START_DATE_DIALOG_ID = 0;
+	static final int END_DATE_DIALOG_ID = 1;
+	private Calendar startDateCalendar = Calendar.getInstance();
+	private Calendar endDateCalendar = Calendar.getInstance();
+
+	// Listener for startDateButton
+	private DatePickerDialog.OnDateSetListener startDateListener = new DatePickerDialog.OnDateSetListener() {
+		// onDateSet method
+		public void onDateSet(DatePicker view, int year, int monthOfYear,
+				int dayOfMonth) {
+			// Set startDateCalendar
+			startDateCalendar.set(year, monthOfYear, dayOfMonth, 0, 0, 0);
+			System.out.println("Start Date: " + startDateCalendar.toString());
+
+		}
+	};
+
+	// Listener fo endDateButton
+	private DatePickerDialog.OnDateSetListener endDateListener = new DatePickerDialog.OnDateSetListener() {
+		// onDateSet method
+		public void onDateSet(DatePicker view, int year, int monthOfYear,
+				int dayOfMonth) {
+			// Set endDateCalendar
+			endDateCalendar.set(year, monthOfYear, dayOfMonth, 23, 59, 59);
+			System.out.println("End Date: " + endDateCalendar.toString());
+
+		}
+	};
+
+	// Listener for all other buttons
+	private simpleListener mySimpleListener = new simpleListener();
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		// Adding buttons to the layout
+		startDateButton = (Button) findViewById(R.id.pickStartDate);
+		// add a click listener to the button
+		startDateButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				showDialog(START_DATE_DIALOG_ID);
+			}
+		});
+
+		endDateButton = (Button) findViewById(R.id.pickEndDate);
+		endDateButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				showDialog(END_DATE_DIALOG_ID);
+			}
+		});
+
+		refreshButton = (Button) findViewById(R.id.refreshGraph);
+		refreshButton.setOnClickListener(mySimpleListener);
+	}
+
+	class simpleListener implements OnClickListener {
+		// 8 visibility = gone
+		// 4 visibility = invisible
+		// 0 visibility = visible
+		@Override
+		public void onClick(View v) {
+			// Refresh the graph and current reading
+			if (v.equals(refreshButton)) {
+				try {
+					refreshGraph();
+				} catch (JSONException e) {
+					Log.e("JSONObject:", e.toString());
+				}
+				refreshCurrent();
+			} else {
+				// No action defined for whatever button called us
+				// Notify with a Toast Popup
+				Toast.makeText(TendrilManager.this,
+						"No action defined for button: " + v.toString(),
+						Toast.LENGTH_SHORT).show();
+
+			}
+		}
+	}
+
+	private void refreshGraph() throws JSONException {
+
+		if (!startDateCalendar.before(endDateCalendar)) {
+			Toast.makeText(TendrilManager.this,
+					"Start date must be before end date!!!", Toast.LENGTH_SHORT)
+					.show();
+			return;
+		}
+
+		JSONObject meterReadings = MeterUtils.getDateRangeReadings(
+				MeterUtils.METER_READINGS_BASE_URL,
+				MeterUtils.calendarToURLString(startDateCalendar, true),
+				MeterUtils.calendarToURLString(endDateCalendar, true));
+
+		JSONObject consumptionReadings = MeterUtils.getDateRangeReadings(
+				MeterUtils.CONSUMPTION_BASE_URL,
+				MeterUtils.calendarToURLString(startDateCalendar, false),
+				MeterUtils.calendarToURLString(endDateCalendar, false));
+
+		
+		System.out.println("meter: " + meterReadings);
+		System.out.println("consumption" + consumptionReadings);
+		// TODO: make graph
+
+	}
+
+	private void refreshCurrent() {
+
+	}
+
+	// Creating dialog
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		Calendar c = Calendar.getInstance();
+		int cyear = c.get(Calendar.YEAR);
+		int cmonth = c.get(Calendar.MONTH);
+		int cday = c.get(Calendar.DAY_OF_MONTH);
+		switch (id) {
+		case START_DATE_DIALOG_ID:
+			return new DatePickerDialog(this, startDateListener, cyear, cmonth,
+					cday);
+		case END_DATE_DIALOG_ID:
+			return new DatePickerDialog(this, endDateListener, cyear, cmonth,
+					cday);
+		}
+		return null;
+	}
+
+	@Override
+	public void onClick(View v) {
+		if (v == startDateButton) {
+			showDialog(START_DATE_DIALOG_ID);
+		} else if (v == endDateButton) {
+			showDialog(END_DATE_DIALOG_ID);
+		}
+	}
+
 }
