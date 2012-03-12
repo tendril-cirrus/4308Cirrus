df984107cb4745cfd4f3ca6f355cedf72b049532
Tyler Howarth <tylerhowarth@gmail.com>
2012-03-11 19:23:05 -0600

    Proper ActionBar Support

--- tendril-example/src/main/java/edu/colorado/cs/cirrus/android/TendrilActivity.java
+++ tendril-example/src/main/java/edu/colorado/cs/cirrus/android/TendrilActivity.java
@@ -14,7 +14,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
-import com.actionbarsherlock;
+import com.actionbarsherlock.app.SherlockActivity;
 
 public class TendrilActivity extends SherlockActivity {
 //	public class TendrilActivity extends FragmentActivity {
