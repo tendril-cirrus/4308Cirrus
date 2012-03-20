eafd7eea15dcdc41cc4e089d31064d10e02eda01
Tyler Howarth <tylerhowarth@gmail.com>
2012-03-20 16:24:30 -0600

    Added Detailed Device Activity

---
+++ tendril-example/src/main/java/edu/colorado/cs/cirrus/android/DeviceDetailActivity.java
@@ -0,0 +1,75 @@
+package edu.colorado.cs.cirrus.android;
+
+import java.util.concurrent.ExecutionException;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import edu.colorado.cs.cirrus.domain.model.Device;
+import edu.colorado.cs.cirrus.domain.model.Devices;
+
+public class DeviceDetailActivity extends AbstractAsyncTendrilActivity {
+	protected static final String TAG = DevicesActivity.class.getSimpleName();
+
+	// private ConnectionRepository connectionRepository;
+	//
+	// private TendrilConnectionFactory connectionFactory;
+	// private Tendril tendril;
+
+	// ***************************************
+	// Activity methods
+	// ***************************************
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.tendril_device_detail_layout);
+
+	}
+
+	@Override
+	public void onStart() {
+
+		super.onStart();
+
+		Devices devices = null;
+		Bundle bundle = this.getIntent().getExtras();
+		int deviceNumber = 0;
+		deviceNumber = bundle.getInt("deviceID");
+		
+		try {
+			//devices = (new DevicesTask()).execute(tendril).get();
+			devices=tendril.asyncGetDevices();
+			
+			Device targetDevice = devices.getDevice().get(deviceNumber);
+			
+			TextView deviceName = (TextView) findViewById(R.id.device_name);
+			deviceName.setText(targetDevice.getName());
+			
+			TextView deviceID = (TextView) findViewById(R.id.device_id);
+			deviceID.setText(targetDevice.getDeviceId());
+			
+			TextView marketingName = (TextView) findViewById(R.id.device_marketing_name);
+			marketingName.setText(targetDevice.getMarketingName());
+			
+			TextView networkID = (TextView) findViewById(R.id.device_network_id);
+			networkID.setText(targetDevice.getNetworkId());
+			
+			
+
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.err.println(devices.toString());
+
+	}
+
+	private boolean isConnected() {
+		return true;
+	}
+}
--- tendril-example/src/main/java/edu/colorado/cs/cirrus/android/DevicesActivity.java
+++ tendril-example/src/main/java/edu/colorado/cs/cirrus/android/DevicesActivity.java
@@ -1,15 +1,18 @@
 package edu.colorado.cs.cirrus.android;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
-import edu.colorado.cs.cirrus.android.task.DevicesTask;
-import edu.colorado.cs.cirrus.domain.model.Devices;
-
-import android.app.Activity;
-import android.os.AsyncTask;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import edu.colorado.cs.cirrus.domain.model.Device;
+import edu.colorado.cs.cirrus.domain.model.Devices;
 
 public class DevicesActivity extends AbstractAsyncTendrilActivity {
 	protected static final String TAG = DevicesActivity.class.getSimpleName();
@@ -25,7 +28,7 @@ public class DevicesActivity extends AbstractAsyncTendrilActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.tendril_generic_text_layout);
+		setContentView(R.layout.tendril_device_list_layout);
 
 	}
 
@@ -38,8 +41,35 @@ public class DevicesActivity extends AbstractAsyncTendrilActivity {
 		try {
 			//devices = (new DevicesTask()).execute(tendril).get();
 			devices=tendril.asyncGetDevices();
-			TextView textView = (TextView) findViewById(R.id.textView1);
-			textView.setText(devices.toString());
+			
+			
+			ListView devicesList = (ListView) findViewById(R.id.devicesList);
+			List<String> deviceNameList = new ArrayList<String>();
+			for (Device d : devices.getDevice()){
+				deviceNameList.add(d.getName());
+			}
+			
+			ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, deviceNameList);
+			devicesList.setAdapter(adapter);
+			
+			
+			devicesList.setOnItemClickListener(new OnItemClickListener() {
+				public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+
+					Intent deviceDetailIntent = new Intent(DevicesActivity.this, DeviceDetailActivity.class);
+					deviceDetailIntent.putExtra("deviceID", position);
+					startActivity(deviceDetailIntent);
+//					Toast.makeText(getApplicationContext(),
+//						"List Item Number: " + (position), Toast.LENGTH_LONG)
+//						.show();
+				}
+			});
+			
+			
+			
+//			TextView textView = (TextView) findViewById(R.id.textView1);
+//			textView.setText(devices.toString());
 
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
