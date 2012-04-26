a6fc2e8b5012a85f6a03d96b0ae4f5fb5746761d
Dan Richelson <drichelson@tendrilinc.com>
2012-04-26 17:08:58 -0600

    updated javadoc

--- tendril-domain/src/main/java/edu/colorado/cs/cirrus/domain/intf/ITendril.java
+++ tendril-domain/src/main/java/edu/colorado/cs/cirrus/domain/intf/ITendril.java
@@ -16,46 +16,89 @@ import edu.colorado.cs.cirrus.domain.model.SetThermostatDataRequest;
 import edu.colorado.cs.cirrus.domain.model.User;
 import edu.colorado.cs.cirrus.domain.model.UserProfile;
 
+/**
+ * Primary interface for accessing Tendril's API: <a href="http://dev.tendrilinc.com">http://dev.tendrilinc.com</a>
+ * 
+ */
 public interface ITendril {
 
-    public abstract String logIn(String userName, String password) throws TendrilException;
+    /**
+     * Authenticates with the Tendril server using provided username and password. Internally sets the access token for
+     * use within the same session Assumes all API keys are set internally
+     * 
+     * @param userName
+     * @param password
+     * @return Access token upon successful login
+     * @throws TendrilException
+     */
 
-    public abstract void useAccessToken(String accessToken);
+    public String logIn(String userName, String password) throws TendrilException;
 
-    // TENDRIL's API is not working well- some date ranges return 500 error for no known reason
-    public abstract CostAndConsumption fetchCostAndConsumption(Resolution resolution, DateTime from, DateTime to,
+    /**
+     * Sets the access token. Useful for applications that do not store a user's credentials, but instead store an
+     * expiring, revokable access token Note: If the <code>logIn()</code> method is called, calling this method with its
+     * return value is not necessary, as the access token is also set upon successful authentication.
+     * 
+     * @param accessToken
+     */
+    public void useAccessToken(String accessToken);
+
+    /**
+     * Fetches Historical Cost and Consumption
+     * 
+     * @param resolution
+     * @param from
+     * @param to
+     * @param limitToLatest
+     * @return
+     * @throws TendrilException
+     */
+    public CostAndConsumption fetchCostAndConsumption(Resolution resolution, DateTime from, DateTime to,
             int limitToLatest) throws TendrilException;
 
-    public abstract CostAndConsumption fetchCostAndConsumptionRange(DateTime from, DateTime to) throws TendrilException;
+    public CostAndConsumption fetchCostAndConsumptionRange(DateTime from, DateTime to) throws TendrilException;
 
-    public abstract Devices fetchDevices() throws TendrilException;
+    public Devices fetchDevices() throws TendrilException;
 
-    public abstract ExternalAccountId fetchExternalAccountId() throws TendrilException;
+    public ExternalAccountId fetchExternalAccountId() throws TendrilException;
 
-    public abstract MeterReadings fetchMeterReadingsRange(DateTime from, DateTime to) throws TendrilException;
+    public MeterReadings fetchMeterReadingsRange(DateTime from, DateTime to) throws TendrilException;
 
-    public abstract PricingProgram fetchPricingProgram() throws TendrilException;
+    public PricingProgram fetchPricingProgram() throws TendrilException;
 
-    // This does not currently work- API documentation is inconsistent
-    public abstract PricingSchedule fetchPricingSchedule(DateTime from, DateTime to) throws TendrilException;
+    /**
+     * 
+     * This does not currently work- API documentation is inconsistent
+     * 
+     * @param from
+     * @param to
+     * @return
+     * @throws TendrilException
+     */
+    public PricingSchedule fetchPricingSchedule(DateTime from, DateTime to) throws TendrilException;
 
-    public abstract User fetchUser() throws TendrilException;
+    public User fetchUser() throws TendrilException;
 
-    // throws a 404 even for current-user. See Tendril's "try it" page
-    public abstract UserProfile fetchUserProfile() throws TendrilException;
+    /**
+     * Throws a 404 even for current-user. See Tendril's "try it" page
+     * 
+     * @return
+     * @throws TendrilException
+     */
+    public UserProfile fetchUserProfile() throws TendrilException;
 
-    public abstract Devices getDevices() throws TendrilException;
+    public Devices getDevices() throws TendrilException;
 
-    public abstract String getExternalAccountId() throws TendrilException;
+    public String getExternalAccountId() throws TendrilException;
 
-    public abstract GetThermostatDataRequest getThermostatData() throws TendrilException;
+    public GetThermostatDataRequest getThermostatData() throws TendrilException;
 
-    public abstract Device getTstat() throws TendrilException;
+    public Device getTstat() throws TendrilException;
 
-    public abstract User getUser() throws TendrilException;
+    public User getUser() throws TendrilException;
 
-    public abstract UserProfile getUserProfile() throws TendrilException;
+    public UserProfile getUserProfile() throws TendrilException;
 
-    public abstract SetThermostatDataRequest setTstatSetpoint(Float setpoint) throws TendrilException;
+    public SetThermostatDataRequest setTstatSetpoint(Float setpoint) throws TendrilException;
 
 }
